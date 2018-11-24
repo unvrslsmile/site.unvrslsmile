1b639a85f2c1c55f0c6394362a096070d4fea994
unvrslsmile <ismaelnapigo@gmail.com>
2018-11-24 22:51:41 +0800

    This is the first initialization of unvrslsmile

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Unvrslsmile</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="./plugs/bootstrap4/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./styles/main-style.css">
+</head>
+<body>
+    <nav class="nav fixed-top navbar-expand-lg navbar-dark bg-transparent-black">
+        <div class="container collapse navbar-collapse" id="tNav">
+            <a class="navbar-brand" href="#">
+                <img src="./images/logo-light.png" alt="" />
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link active btn btn-success" href="#">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sign In</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+       
+    </div>
+</body>
+<script src="./plugs/jQuery/jquery-3.3.1.min.js"></script>
+<script src="./plugs/bootstrap4/js/bootstrap.min.js"></script>
+</html>
